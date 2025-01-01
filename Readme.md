7cd9d51c9de1102a2f4dcd36ed4f934166d26fe0
Singh564 <vsbora77@gmail.com>
2025-01-01 18:18:01 +0530

    add main game fles

---
+++ Readme.md
@@ -0,0 +1,3 @@
+### In This project we have made use of python's Random Module to get a Random word from a list
+
+### Executed a while loop over to take input from the user until the Gussed word equals random word or total chances get zero.
---
+++ main.py
@@ -0,0 +1,49 @@
+import random
+
+#Make list of random words 
+Word_Group = ['money','monkey','asia','docs','domain','roman']
+
+#Get a random word using the list of word group
+Random_Word= random.choice(Word_Group)
+
+
+Gussed_Word = '_'*len(Random_Word)
+print(Gussed_Word)
+Total_Chances= 6
+
+
+while True:
+    #Take input from the user
+    Gussed_letter = input('Enter The Letter ').lower()
+    if Gussed_letter in Random_Word:
+        for idx in range(len(Random_Word)):
+            if Gussed_letter== Random_Word[idx]:
+                Gussed_Word= Gussed_Word[:idx] + Gussed_letter + Gussed_Word[idx+1:]
+
+        
+        if Gussed_Word == Random_Word:
+                print(' Hurrey,You have won the game')
+                print(f"Correct Word is {Random_Word}")
+                break
+
+        else:
+            print(f"Correct,Guess {Gussed_Word}")
+            
+
+    else:
+        Total_Chances-=1
+        print( f"Incorrect Guess, You have {Total_Chances} chances left ")
+
+        if Total_Chances==0:
+            print('You have LOST thE GAME')
+            break
+
+    
+
+
+
+
+
+
+
+
